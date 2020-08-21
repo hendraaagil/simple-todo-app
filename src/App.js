1f7a83ace02200eca049af2ae469f3cd00a13d21
hendra-agil <agilsyaputra26@gmail.com>
2020-08-21 20:58:04 +0700

    :hammer: Add delete operation

--- src/App.js
+++ src/App.js
@@ -10,11 +10,20 @@ class App extends Component {
     ],
   };
 
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos,
+    });
+  };
+
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
--- src/Todos.js
+++ src/Todos.js
@@ -1,16 +1,22 @@
 import React from 'react';
 
-const Todos = ({ todos }) => {
+const Todos = ({ todos, deleteTodo }) => {
   const todoList = todos.length ? (
     todos.map((todo) => {
       return (
         <div className="collection-item" key={todo.id}>
-          <span>{todo.content}</span>
+          <span
+            onClick={() => {
+              deleteTodo(todo.id);
+            }}
+          >
+            {todo.content}
+          </span>
         </div>
       );
     })
   ) : (
-    <p className="center">You have no todo's left</p>
+    <p className="center">You have no todo's left. Yay!</p>
   );
 
   return <div className="todos collection">{todoList}</div>;
